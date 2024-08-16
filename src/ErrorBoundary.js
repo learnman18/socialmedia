11ff17166ad4b8b355de325764612c675748a992
learnman18 <manishpan18@gmail.com>
2024-08-16 22:25:11 +0530

    [16-8]-errorBoundry

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state = {hasError : false}
+    // }
+    state = {hasError : false}
+
+    static getDerivedStateFromError(error){
+        return {hasError : true}
+    }
+
+    componentDidCatch(error, errorInfo){
+        console.log("componentDidCatch", error, errorInfo);
+        // logErrorToMyService(error, errorInfo);
+    }
+    componentDidMount(error, errorInfo){
+        console.log(error, errorInfo);
+    }
+
+    render(){
+        if(this.state.hasError){
+            return this.props.fallback
+        }
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary;
