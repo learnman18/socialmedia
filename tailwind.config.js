7cecfb8803415354c040d95777459c98868a8b5e
learnman18 <manishpan18@gmail.com>
2024-08-16 21:37:48 +0530

    [16-8]-userTable

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: {max: '768px'},
+      md: {min : '768px' , max: '1024px'},
+      lg: {min: '1024px' , max:'1280px'},
+      xl: {min: '1280px'}
+    },
   },
   plugins: [],
 }
