2b3f95e491ce7dddd6340ee9f481f94c02d757e0
learnman18 <manishpan18@gmail.com>
2024-09-11 13:20:28 +0530

    [11-9]-addedUserPostCommentUI

--- src/components/users/IndividualUserPost.js
+++ src/components/users/IndividualUserPost.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { IndividualPostAction } from "../actions/IndividualPostAction";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -8,17 +8,30 @@ const IndividualUserPost = ()=> {
     const dispatch = useDispatch();
     const { postComment, error } = useSelector((item)=> item.userCommentPost );
     const { postCommentId } = useParams();
+    const [userPostComment, setUserPostComment] = useState();
 
     useEffect(()=>{
         console.log("post page id", postCommentId)
         dispatch(IndividualPostAction(parseInt(postCommentId)));
     },[dispatch, postCommentId]);
 
-    console.log("post comment", postComment)
+    useEffect(()=>{
+        setUserPostComment(postComment);
+    },[postComment])
+
+    console.log("post comment", userPostComment);
 
     return (
         <>
-            <p>Commetns</p>
+            { userPostComment && 
+                userPostComment.map((item)=>(
+                    <div key={item.id}>
+                        <p>{item.name}</p>
+                        <p>{item.body}</p>
+                        <p>{item.email}</p>
+                    </div>
+                ))
+            }
         </>
     )
 }
